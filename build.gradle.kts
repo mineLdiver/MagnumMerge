028b74d5a3f0c38e91152b1e52fb4b2a77aa5b68
mineLdiver <aabesedin@mail.ru>
2024-01-03 18:19:33 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "net.mine_diver"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("jdiff:jdiff:1.0.9")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
